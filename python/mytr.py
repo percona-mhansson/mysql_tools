e1b142f8c02646dda0bb1eea8bb2e6515db5e349
Martin Hansson <martinhson@gmail.com>
2026-04-23 11:25:42 +0200

    More build flags to mytr

--- python/mytr.py
+++ python/mytr.py
@@ -24,17 +24,43 @@ def main():
         help="verbose",
     )
 
+    parser.add_argument(
+        "-H",
+        "--build-home",
+        default=mysql.Defaults.BUILD_HOME,
+        help="home directory for mysql builds (default: %(default)s)",
+    )
+
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="don't actually start mysqld, only print how it would have started and then exit",
+    )
+
+    build_specific_args = parser.add_mutually_exclusive_group()
+
+    build_specific_args.add_argument(
+        "-B",
+        "--build-dir",
+        help="lets you specify the build directory directly, instead of inferring it from the "
+        "build type (see --build-type)",
+    )
+
     args, mtr_args = parser.parse_known_args()
 
     if args.verbose >= 2:
         print(f"mtr args: {" ".join(mtr_args)}")
 
-    cwd = os.path.abspath(
-        f"{mysql.Defaults.BUILD_HOME}/{mysql.Defaults.BUILD_TYPE}/mysql-test"
-    )
+    build = mysql.Build("args.workdir", args.build_dir, mysql.Defaults.BUILD_TYPE)
 
-    exe = f"{cwd}/{MTR}"
+    cwd = os.path.abspath(f"{build.build_dir}/mysql-test")
+
+    exe = f"{build.build_dir}/mysql-test/{MTR}"
     mtr_args = [exe] + mtr_args
+
+    if args.dry_run:
+        print(f"Would run {MTR} like this: {" ".join(mtr_args)}")
+        sys.exit(0)
 
     if args.verbose:
         print(f"Running {MTR} like this: {" ".join(mtr_args)}")
