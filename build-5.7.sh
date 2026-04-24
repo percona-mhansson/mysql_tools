7cd511313a413173edd6c2495dd27788e5206360
Martin Hansson <martinhson@gmail.com>
2026-04-24 09:35:46 +0200

    asan

--- build-5.7.sh
+++ build-5.7.sh
@@ -1,7 +1,18 @@
+asan=1
 build_type=${1:-Debug}
-build_dir=build/${build_type,,}
 
-cmake -B "$build_dir" -GNinja -DCMAKE_BUILD_TYPE=$build_type \
-     -DDOWNLOAD_BOOST=1 -DWITH_BOOST=~/boost
+build_dir=build.dir/${build_type,,}
 
-time ninja -C "$build_dir"
+if [ "$asan" == 1 ]; then
+  asan_opt="-DWITH_ASAN=ON"
+  build_dir="$build_dir.asan"
+fi
+
+cmd="cmake -B ${build_dir} -GNinja -DCMAKE_BUILD_TYPE=$build_type \
+      -DDOWNLOAD_BOOST=1 -DWITH_BOOST=~/boost \
+     $asan_opt"
+
+echo $cmd
+
+${cmd} && time ninja -C "$build_dir"
+
